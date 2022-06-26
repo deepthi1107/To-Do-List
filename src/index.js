2a302cb06ef4615dc68769c49d560c775b1ef97f
Deepthi M <79050917+deepthi1107@users.noreply.github.com>
2022-06-26 18:37:25 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Todolist from './components/Todolist';
+import './index.css';
+
+ReactDOM.render(<Todolist />, document.getElementById('root'));
